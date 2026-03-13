e841d664103202f6bf284a4795e5e1149f80373e
rhamenator <rhamenator@gmail.com>
2026-03-12 21:54:49 -0400

    Revert "Merge pull request #1 from rhamenator/codex/plan-architectural-refactor-for-iis-repo"
    This reverts commit 386c531b6b8f2d9d0c8b6f8223e335d632982c27, reversing
    changes made to 5d58fc9c6f1eb06fe5c080a909014437e241a728.

--- RedisBlocklistMiddlewareApp/Services/IPolicyService.cs
+++
@@ -1,9 +0,0 @@
-using RedisBlocklistMiddlewareApp.Models;
-
-namespace RedisBlocklistMiddlewareApp.Services;
-
-public interface IPolicyService
-{
-    Task<PolicySubmissionResponse> PushPolicyAsync(PolicySubmissionRequest request, CancellationToken cancellationToken = default);
-    Task<ServiceOperationResult> SyncPendingPolicyChangesAsync(CancellationToken cancellationToken = default);
-}
